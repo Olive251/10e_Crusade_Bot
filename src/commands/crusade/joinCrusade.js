147cd19c70c976f48c9bfbc73fa7598ee3ea6d1c
Olivia Brown <oliviataylor437@gmail.com>
2023-05-10 15:37:34 -0400

    first pass on join-crusade

--- src/commands/crusade/joinCrusade.js
+++ src/commands/crusade/joinCrusade.js
@@ -10,6 +10,20 @@ module.exports = {
                 .setDescription(`The name of the crusade you wish to join`)),
     run: async ({interaction}) => {
         
+        if (!interaction.options.get('crusade-name').value){
+            interaction.reply(`Optionless version not yet implemented!`);
+            return;
+        }
+        await interaction.deferReply();
+        const user = interaction.options.getUser('user') || interaction.user;
+        const member = await interaction.guild.members.fetch(user.id);
+
+        let crusade = await Crusade.findOne({name: interaction.options.get('crusade-name').value, guildID: interaction.guildId});
+        console.log(crusade);
+        crusade.players.push(user.id);
+        await Crusade.updateOne({_id:crusade._id}, {players: crusade.players});
+
+        interaction.editReply(`You have sucessfully joined the ${crusade.name}!`);
     }
             
 }
