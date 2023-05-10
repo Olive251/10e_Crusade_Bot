07a9566b359b2323d1e323d3ce70839284263b1c
Olivia Brown <oliviataylor437@gmail.com>
2023-05-10 16:36:26 -0400

    modified

--- src/commands/crusade/joinCrusade.js
+++ src/commands/crusade/joinCrusade.js
@@ -17,13 +17,29 @@ module.exports = {
         await interaction.deferReply();
         const user = interaction.options.getUser('user') || interaction.user;
         const member = await interaction.guild.members.fetch(user.id);
+        var crusade;
+        try{
+            crusade = await Crusade.findOne({name: interaction.options.get('crusade-name').value, guildID: interaction.guildId});
+            if (!crusade){
+                interaction.editReply(`${interaction.options.get('crusade-name').value} was not found among this server's crusade records. Please ensure you use the correct name.`);
+                return;
+            };
+        } catch (err){
+            console.log(`ERROR in joinCrusade:\n${err}`);
+            interaction.editReply(`++Unable to join the crusade. Problem communing with the deeper machine spirits.++`);
+            return;
+        }
+        
+        if (!crusade.players.includes(user.id)){
+            crusade.players.push(user.id);
+            await Crusade.updateOne({_id:crusade._id}, {players: crusade.players});
 
-        let crusade = await Crusade.findOne({name: interaction.options.get('crusade-name').value, guildID: interaction.guildId});
-        console.log(crusade);
-        crusade.players.push(user.id);
-        await Crusade.updateOne({_id:crusade._id}, {players: crusade.players});
-
-        interaction.editReply(`You have sucessfully joined the ${crusade.name}!`);
+            interaction.editReply(`You have sucessfully joined the ${crusade.name}!`);
+        }
+        else {
+            interaction.editReply(`You're already in the ${crusade.name}, you silly goose!`);
+        }
+        
     }
             
 }
