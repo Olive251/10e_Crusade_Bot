d19640575ba1ffe5a76d5a377d500cd08775d835
Olivia Brown <oliviataylor437@gmail.com>
2023-05-10 07:48:54 -0400

    complete add_crusade, work on find

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const { Client, IntentsBitField } = require('discord.js');
 require('dotenv').config();
 const {connect_db} = require('./data/mongo-connection.js');
+const {register_crusade, find_crusade} = require('./cmd-functs/crusade.js');
 
 const client = new Client({
     intents: [
@@ -18,12 +19,36 @@ client.on('ready', (c) => {
     console.log(`✅ - ${c.user.tag} is online! Huzzah! - ✅`);
 })
 
-client.on('interactionCreate', (interaction) => {
+client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
+
+    let guildID = interaction.guildId;
 
     switch(interaction.commandName) {
         case 'echo':
             interaction.reply(interaction.options.get('echo-content').value);
+            break;
+        case 'new-crusade':
+            let name = interaction.options.get('crusade-name').value;
+            var crusade 
+
+            try{
+               crusade = await register_crusade(name, guildID);
+               interaction.reply(`The "${crusade.name} was successfully created!`);
+            } catch (err){
+                interaction.reply(`ERROR: Could not create "${name}"`);
+            }
+
+            break;
+        case 'find-crusade':
+            let searchCrusade = interaction.options.get();
+            crusade = await find_crusade(guildID, searchCrusade);
+
+            if (crusade)
+                interaction.reply(crusade);
+            else
+                interaction.reply(`Unable to locate crusade...`);
+
             break;
         default:
             return;
