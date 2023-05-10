c6cf63327a0167853a910495627ff342341a3a0d
Olivia Brown <oliviataylor437@gmail.com>
2023-05-10 18:22:38 -0400

    buttons added to listCrusade embed

--- src/commands/crusade/listCrusades.js
+++ src/commands/crusade/listCrusades.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js');
 const {Crusade} = require('../../data/schemas');
 
 module.exports = {
@@ -15,15 +15,24 @@ module.exports = {
                 .setTitle(`Crusades Report`)
                 .setDescription(`Crusades available on the ${interaction.guild.name} server`)
                 .setColor('Random');
+
+            const optRow = new ActionRowBuilder();
             
             for (const crusade of crusades){
                 embed.addFields(
                     {name: `🔸 ${crusade.name}`, value: ' '}
                 )
+
+                optRow.addComponents(
+                    new ButtonBuilder()
+                    .setCustomId(crusade.name.toLowerCase() + '-selector')
+                    .setLabel(crusade.name)
+                    .setStyle(ButtonStyle.Primary),
+                )
             }
 
             if (crusades) 
-                interaction.editReply({embeds: [embed]});
+                interaction.editReply({embeds: [embed], components: [optRow]});
             else
                 interaction.editReply(`❗ There are no active crusades for this server`)
 
