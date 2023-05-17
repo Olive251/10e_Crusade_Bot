a9cba039461e35f0a71d006d1c342b70c6dad511
Olivia Brown <oliviataylor437@gmail.com>
2023-05-17 11:33:17 -0400

    completed join crusade btn

--- src/events/interactionCreate/buttons/btn_joinCrusade.js
+++ src/events/interactionCreate/buttons/btn_joinCrusade.js
@@ -9,14 +9,15 @@ module.exports = async (interaction) => {
         let searchCrusadeId = interaction.customId.substr(13, interaction.customId.length);
         var crusade;
         try{
-            crusade = Crusade.findOne({_id: searchCrusadeId});
+            crusade = await Crusade.findOne({_id: searchCrusadeId});
         } catch (err) {
             console.log(`Error finding crusade ${searchCrusadeId}\n ${err}`);
             interaction.reply(`❗ - There was a problem joining the ${crusade.name}`)
             return;
         }
+        console.log(interaction.user.id)
 
-        let result = addUserToCrusade(crusade, interaction.user);
+        let result = addUserToCrusade(crusade, interaction.user.id);
 
         if (result){
             interaction.reply(`✅ - You have sucessfully joined the ${crusade.name}`);
